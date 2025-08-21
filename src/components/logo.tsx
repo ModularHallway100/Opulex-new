ce3305c9f4a1b7d91fce76c713ad922eba30bb59
SAI ALAPATI <saialapati0508@gmail.com>
2025-08-21 10:20:19 +0000

    Can you put Logo.png which is already in the project as the main Opulex

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Logo = ({ className }: { className?: string }) => (
   <div className="flex items-center">
-    <Image src="/logo.png" alt="Opulex Logo" width={140} height={40} className={className} />
+    <Image src="/Logo.png" alt="Opulex Logo" width={140} height={40} className={className} priority />
   </div>
 );
 
